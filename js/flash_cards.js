fbba955add8434d1a79ff6575f00e03aceef7a66
R. W. Sinnet <ryan@rwsinnet.com>
2015-07-28 21:02:26 -0700

    Adding fontawesome, adding counters and indicators to flashcard game.

--- js/flash_cards.js
+++ js/flash_cards.js
@@ -1,15 +1,46 @@
+var nCorrect = 0;
+var nIncorrect = 0;
+
+var correctAnswer;
+
+var shuffledVerbList = _.shuffle(verbsXXX);
+var verbIndex = 0;
+var repeat = false;
+
+var verbsMin = 0;
+var verbsMax = verbsXXX.length - 1;
+
+var correctDict = {};
+var incorrectDict = {};
+
 var updateFlashDiv = function() {
-    var min = 0;
-    var max = verbsXXX.length - 1;
-    var random = Math.floor(Math.random() * (max - min + 1)) + min;
-    var verb = verbsXXX[random];
+
+    var verb;
+
+    if (!repeat)
+	verb = shuffledVerbList[verbIndex++];
+    else
+    {
+	var correctRandom = Math.floor(Math.random()
+				       * (verbsMax - verbsMin + 1)) + verbsMin;
+	verb = verbsXXX[correctRandom];
+    }
+
+    correctAnswer = verb;
 
     verbs = {};
     verbs[0] = verb;
 
-    for (var i = 1; i < 4; i++) {
-	random = Math.floor(Math.random() * (max - min + 1)) + min;
-	verbs[i] = verbsXXX[random];
+    var i = 0;
+    while (i < 4)
+    {
+	random = Math.floor(Math.random()
+			    * (verbsMax - verbsMin + 1)) + verbsMin;
+
+	if (repeat && random == correctRandom || !repeat && random == verbIndex)
+	    continue;
+
+	verbs[++i] = verbsXXX[random];
     }
 
     var divElem = document.createElement("div");
@@ -20,10 +51,34 @@ var updateFlashDiv = function() {
 
     var trElem = document.createElement("tr");
     var tdElem = document.createElement("td");
-    tdElem.setAttribute("colspan", 2);
     tdElem.innerText = verb.root;
     tdElem.title = verb.translation;
     tdElem.className = "verb-root-cell-header";
+    trElem.appendChild(tdElem);
+
+    tdElem = document.createElement("td");
+    tdElem.className = "flash-card-score";
+    var spanElem = document.createElement("span");
+    spanElem.className = "fa fa-thumbs-up fa-lg flash-card-icon color-correct";
+    tdElem.appendChild(spanElem);
+
+    spanElem = document.createElement("span");
+    spanElem.className = "flash-card-score";
+    spanElem.innerText = nCorrect;
+    tdElem.appendChild(spanElem);
+    trElem.appendChild(tdElem);
+
+    tdElem = document.createElement("td");
+    tdElem.className = "flash-card-score";
+    spanElem = document.createElement("span");
+    spanElem.className =
+	"fa fa-thumbs-down fa-lg flash-card-icon color-incorrect";
+    tdElem.appendChild(spanElem);
+
+    spanElem = document.createElement("span");
+    spanElem.className = "flash-card-score";
+    spanElem.innerText = nIncorrect;
+    tdElem.appendChild(spanElem);
     trElem.appendChild(tdElem);
 
     tableElem.appendChild(trElem);
@@ -31,14 +86,19 @@ var updateFlashDiv = function() {
     verbIndices = _.shuffle(_.range(4));
 
     for (var i = 0; i < 4; i++) {
+	var thisVerb = verbs[verbIndices[i]];
 	trElem = document.createElement("tr");
 
 	tdElem = document.createElement("td");
 
-	var textBox = document.createElement("button");
-	textBox.className = "hebrew-translation-button";
-	textBox.innerHTML = verbs[verbIndices[i]].translation;
-	tdElem.appendChild(textBox);
+	var button = document.createElement("button");
+	button.className = "hebrew-translation-button";
+	button.innerHTML = thisVerb.translation;
+	button.setAttribute("onclick", "doAnswerCheck(this, \""
+			    + thisVerb.root + "\")");
+
+	tdElem.setAttribute("colspan", 3);
+	tdElem.appendChild(button);
 	trElem.appendChild(tdElem);
 	tableElem.appendChild(trElem);
     }
@@ -46,6 +106,34 @@ var updateFlashDiv = function() {
     return divElem;
 }
 
-$(document).ready(function() {
+function doAnswerCheck(button, answer)
+{
+    _.each($(".hebrew-translation-button"), function(button) {
+	button.disabled = true;
+    });
+
+    if (answer == correctAnswer.root) {
+	button.className = button.className + " bgcolor-correct";
+	++nCorrect;
+	if (!(correctAnswer.root in correctDict))
+	    correctDict[correctAnswer.root] = 0;
+	++correctDict[correctAnswer.root];
+    } else {
+	button.className = button.className + " bgcolor-incorrect";
+	++nIncorrect;
+	if (!(correctAnswer.root in incorrectDict))
+	    incorrectDict[correctAnswer.root] = 0;
+	++incorrectDict[correctAnswer.root];
+    }
+
+    setTimeout(nextFlashCard, 2000);
+}
+
+function nextFlashCard()
+{
     $('#flashDiv').html(updateFlashDiv());
+}
+
+$(document).ready(function() {
+    nextFlashCard();
 });
